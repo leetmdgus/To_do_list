967f3af6123d3328d636fc0e51e1143a7fe70ea5
leetmdgus <lee_tmdgus@naver.com>
2022-05-27 11:25:03 +0900

    add d-day function

--- js/d-day.js
+++ js/d-day.js
@@ -1,23 +1,34 @@
 const span =document.querySelector('#d-day span');
 const input = document.querySelector('#d-day input')
 
-function getDday(event)
-{
-  event.preventDefault();
-  const wantKnowYear = document.querySelector('#year');
-  const wantKnowMonth = document.querySelector('#month');
-  const wantKnowDate = document.querySelector('#date');
-
-  const date = new Date();
-  const wanKnowDate = new Date(wantKnowYear.value,wantKnowMonth.value,wantKnowDate.value);
-
-  const timeDiff = wantKnowDate.getTime()-date.getTime();
-
-  const dDay = timeDiff/1000/60/60/24;
-
-  span.innerText = dDay;
-  console.log(dDay);
-  alert(dDay)
+function diff(date1,date2,operator){
+  let timeDiff;
+  let dDay;
+  timeDiff = date1-date2;
+  dDay = timeDiff / 1000 / 60 / 60 / 24;
+  dDay = Math.floor(dDay);
+  span.innerText = `${operator}${dDay}`;
 }
 
-span.addEventListener("submit",getDday);
+function getDday(event)
+{
+  event.preventDefault();const inputYear = document.querySelector('#year');
+  const inputMonth = document.querySelector('#month');
+  const inputDate = document.querySelector('#date');
+  
+  const year = Number(inputYear.value);
+  const month = Number(inputMonth.value)-1;
+  const date = Number(inputDate.value);
+  
+  const userDate = new Date(year, month, date);
+  const nowDate = Date.now();
+  
+  if(userDate>=nowDate){
+    diff(userDate,nowDate,'-');
+  } else if(userDate<nowDate){
+    diff(nowDate,userDate,'+');
+  }
+}
+
+window.addEventListener("submit",getDday);
+
